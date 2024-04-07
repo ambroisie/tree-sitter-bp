38bf9532ad64909a1d6ec282306e335e8225235e
Bruno BELANYI <bruno@belanyi.fr>
2024-04-07 21:37:52 +0100

    Add assignment

--- grammar.js
+++ grammar.js
@@ -1,9 +1,41 @@
 module.exports = grammar({
   name: "blueprint",
 
+  extras: ($) => [
+    /\s+/,
+  ],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => 'hello',
+    source_file: ($) => repeat($._definition),
+
+    _definition: ($) => choice(
+      $.assignment,
+    ),
+
+    // Definitions {{{
+
+    assignment: ($) => seq(
+      field("left", $.identifier),
+      field("operator", "="),
+      field("right", $._expr),
+    ),
+
+    // }}}
+
+    // Expressions {{{
+
+    _expr: ($) => choice(
+      // Literals
+      $.identifier,
+      $.integer_literal,
+    ),
+
+    // The Blueprint scanner makes use of Go's lexer, so copy their rule
+    identifier: (_) => /[_\p{XID_Start}][_\p{XID_Continue}]*/,
+
+    integer_literal: (_) => /[0-9]+/,
+
+    // }}}
   }
 });
 
