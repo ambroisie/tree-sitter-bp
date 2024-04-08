b9fd34c084b49bab7a8e28265adc6d0d89191200
Bruno BELANYI <bruno@belanyi.fr>
2024-04-08 20:47:55 +0100

    Add literals highlighting

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -3,4 +3,17 @@
  (block_comment)
 ] @comment
 
+; Literal {{{
+(boolean_literal) @boolean
+
+(integer_literal) @number
+
+[
+ (raw_string_literal)
+ (interpreted_string_literal)
+] @string
+
+(escape_sequence) @string.escape
+; }}}
+
 ; vim: sw=2 foldmethod=marker
