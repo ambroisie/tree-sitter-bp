fd92a31674044737fa96a152181da3a0f9db8054
Bruno BELANYI <bruno@belanyi.fr>
2024-12-23 17:48:08 -0500

    Fix typos

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_bp::language()).expect("Error loading txtpb grammar");
+//! parser.set_language(tree_sitter_bp::language()).expect("Error loading bp grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
