bb0d5d6a406e81b61bf587d3839d75d326ce7128
Bruno BELANYI <bruno@belanyi.fr>
2025-03-12 21:24:44 +0000

    Release 0.6.0

--- src/parser.c
+++ src/parser.c
@@ -4286,7 +4286,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_bp(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 5,
+      .minor_version = 6,
       .patch_version = 0,
     },
   };
